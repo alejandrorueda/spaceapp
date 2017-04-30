de4aa4c6c6a719e74ddb45a8c094781f33614431
Pablo Palazón <ppalazon@antara.ws>
2017-04-30 10:44:25 +0200

    Create js from json

--- pollution-city-all-data.sh
+++ pollution-city-all-data.sh
@@ -9,3 +9,6 @@ script="pollution-city.py --cities=$folder/pollution-by-city.json --output=$fold
 python $script --nc=pollution-data/g4.timeAvgMap.GLDAS_NOAH10_M_2_1_Lwnet_tavg.20101130-20170130.180W_60S_180E_90N.nc --variable=GLDAS_NOAH10_M_2_1_Lwnet_tavg --attr=lw_flux
 python $script --nc=pollution-data/g4.timeAvgMap.M2TMNXCHM_5_12_4_COEM.20101130-20170130.180W_90S_180E_90N.nc --variable=M2TMNXCHM_5_12_4_COEM --attr=co
 python $script --nc=pollution-data/g4.timeAvgMap.AIRS3C2M_005_mole_fraction_of_carbon_dioxide_in_free_troposphere.20101130-20170130.180W_60S_180E_90N.nc --variable=AIRS3C2M_005_mole_fraction_of_carbon_dioxide_in_free_troposphere --attr=co2
+
+echo 'var citymap=' > $folder/pollution-by-city.js
+cat $folder/pollution-by-city.json >> $folder/pollution-by-city.js
