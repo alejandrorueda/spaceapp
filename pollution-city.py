ca13ae0bcf3c0bd8883f767573258f86fc97322c
Pablo Palazón <ppalazon@antara.ws>
2017-04-30 13:51:17 +0200

    Add percent value for each measure

--- pollution-city.py
+++ pollution-city.py
@@ -21,14 +21,19 @@ with open(cities, 'r') as infile:
     cities = json.loads(infile.read())
 
 dataset = Dataset(filename, 'r')
+limit = {
+    'min': None,
+    'max': None
+}
 
 def atributos(group):
 	for name in group.ncattrs():
 		print "Global attr", name, "=", getattr(group,name)
-def location(city):
+
+def location(city, limit):
     indlat=0
     indlon=0
-    city[attr] = 'error'
+    city[attr] = None
     lat1 = dataset.variables['lat']
     lon1 = dataset.variables['lon']
     center = city['center']
@@ -39,11 +44,30 @@ def location(city):
         if latitud < center['lat']:
             indlat+=1
     print variable + ' ' + city['city']
+    print limit
     if indlon < lon1[:].size and indlat < lat1[:].size :
         city[attr]=dataset.variables[variable][indlat][indlon].tolist()
+        if(city[attr]!=None) :
+            if(limit['min']==None or city[attr] < limit['min']) :
+                limit['min'] = city[attr]
+            if(limit['max']==None or city[attr] > limit['max']) :
+                limit['max'] = city[attr]
+
+def percentlocation(city, limit):
+    value = city[attr]
+    if(value != None):
+        print 'Percent values: '+variable + ' ' + city['city']
+        if (limit['max'] - limit['min']) > 0 :
+            city[attr+'_per'] = abs(((city[attr] - limit['min']) * 100) / (limit['max'] - limit['min']))
+        else :
+            city[attr+'_per'] = 100
+        print city[attr] + ' ' + city[attr+'_per']
 
 for city in cities.values():
-    location(city)
+    location(city, limit)
+
+for city in cities.values():
+    percentlocation(city, limit)
 
 with open(output, 'w') as outfile:
     json.dump(cities, outfile, sort_keys=True, indent=2)
