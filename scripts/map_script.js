7a15a977f020dcc318b7cec55760114f124e4a98
Pablo Palazón <ppalazon@antara.ws>
2017-05-04 22:22:22 +0200

    Change json url

--- scripts/map_script.js
+++ scripts/map_script.js
@@ -112,7 +112,7 @@ function load_earthquakes(){
 	// Create a <script> tag and set the USGS URL as the source.
 	var script = document.createElement('script');
 
-	script.src = 'http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp';
+	script.src = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp';
 	document.getElementsByTagName('head')[0].appendChild(script);
 
 	map.data.setStyle(function(feature) {
